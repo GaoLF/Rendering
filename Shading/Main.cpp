b56a4a4e91bb2119e63b6b464546fd36e0e2e924
Gao <Gaolongfei@pku.edu.cn>
2018-11-05 19:38:47 +0800

    create the opengl common files

---
+++ Shading/Main.cpp
@@ -0,0 +1,24 @@
+#define GLEW_STATIC
+#include "glad\glad.h"
+#include "glfw3.h"
+#include <iostream>
+// GLEW
+
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	return 0;
+}
